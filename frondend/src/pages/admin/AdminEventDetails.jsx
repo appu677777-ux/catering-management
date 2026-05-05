455f314d0d387925b6bd317073a88756af210a2c
appu677777-ux <appu677777@gmail.com>
2026-05-05 13:30:19 +0530

    image linkedd

--- frondend/src/pages/admin/AdminEventDetails.jsx
+++ frondend/src/pages/admin/AdminEventDetails.jsx
@@ -14,7 +14,7 @@ export default function AdminEventDetails() {
 
   const BASE_URL = import.meta.env.VITE_API_URL
     ? import.meta.env.VITE_API_URL.replace("/api", "")
-    : "http://localhost:5000";
+    : "https://catering-management-1.onrender.com";
 
   // 🔥 FETCH EVENT
   useEffect(() => {
