4786c014a157c873fde0e7eaa4f258dde7961bd1
appu677777-ux <appu677777@gmail.com>
2026-05-05 13:20:57 +0530

    image link

--- frondend/src/components/admin/AdminEventCard.jsx
+++ frondend/src/components/admin/AdminEventCard.jsx
@@ -8,7 +8,7 @@ export default function AdminEventCard({ event, onDelete }) {
 
     const BASE_URL = import.meta.env.VITE_API_URL
         ? import.meta.env.VITE_API_URL.replace("/api", "")
-        : "http://localhost:5000";
+        : "https://catering-management-1.onrender.com/api";
 
     return (
         <div className="bg-white shadow-md rounded-xl overflow-hidden hover:shadow-lg transition">
--- frondend/src/services/api.js
+++ frondend/src/services/api.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api"
+  baseURL: "https://catering-management-1.onrender.com/api"
 });
 
 // attach token
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
   if (token) {
-    req.headers.Authorization = `Bearer ${token}`; 
+    req.headers.Authorization = `Bearer ${token}`; // ✅ FIXED
   }
   return req;
 });
